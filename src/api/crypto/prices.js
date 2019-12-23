35d416aeaa803465987c273502e6af2740710b2f
Filippo Minutella <filippominutella@gmail.com>
2019-12-23 16:53:28 +0100

    rewrite history api

--- src/api/crypto/prices.js
+++ src/api/crypto/prices.js
@@ -1,22 +1,21 @@
-/* eslint-disable no-param-reassign */
 const { NUM_OF_HISTORY_PRICE } = require('../../config');
 const CryptoPrice = require('../../model/CryptoPrice');
+const mongoose = require('mongoose');
 
 module.exports = async (crypto, id) => {
   let prices;
   if (id) {
-    prices = await CryptoPrice.find(
-      { crypto, _id: { $lt: id } },
-      null,
-      { sort: { createdAt: 1 }, limit: NUM_OF_HISTORY_PRICE }
-    );
+    prices = await CryptoPrice.find({
+      crypto,
+      _id: { $lt: mongoose.Types.ObjectId(id) }
+    })
+      .sort({ createdAt: -1 })
+      .limit(NUM_OF_HISTORY_PRICE);
   } else {
-    prices = await CryptoPrice.find(
-      { crypto },
-      null,
-      { sort: { createdAt: 1 }, limit: NUM_OF_HISTORY_PRICE }
-    );
+    prices = await CryptoPrice.find({ crypto })
+      .sort({ createdAt: -1 })
+      .limit(NUM_OF_HISTORY_PRICE);
   }
 
-  return prices.map(cryptoPrice => cryptoPrice.price);
+  return prices.map(price => price.price).reverse();
 };
